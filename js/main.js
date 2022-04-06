cf85a6392c0d5e8811c90672207fbfcb16caf379
Bibaxify <caillarda@mac.com>
2022-04-06 11:47:12 +0200

    update index.html

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// déclarer les variables qui vont recevoir le canvas et le context
+
+
+// fonction getMouseLocation(e) qui va retourner la position de la souris
+
+
+
+
+
+// fonction onClickCanvas(e) qui va récuperer la position du clic et appeler differente méthodes pour génerer le dessin
+
+
+
+
+// au chargement de la page, on va récupérer le canvas, le context et installer un écouteur 
---
+++ js/utilities.js
@@ -0,0 +1,3 @@
+// fonction getRandomColor va retourner une string avec des valeurs rgba
+
+// fonction getRandomInteger
